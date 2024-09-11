69adc0f4138ccd70f0f07cc66353c402e451ed87
matheusmartinsviana <matheusmviana@outlook.com.br>
2024-09-11 09:54:54 -0300

    feat: add initial express config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("helloworld")
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get('/',(req,res) => {
+    res.send("hello world")
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
